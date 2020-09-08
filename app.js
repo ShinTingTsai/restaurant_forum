291427a0243cab0df6620279ebf049ec767ad127
STTSAI <shinting.tsai@gmail.com>
2020-09-09 02:39:42 +0800

    feat: add user signup function

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')

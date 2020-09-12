302d24c0adb89545f03d6f1c5439fec2ef6a6425
STTSAI <shinting.tsai@gmail.com>
2020-09-12 12:42:17 +0800

    feat: add create put page and install method-override

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,10 @@ module.exports = (app, passport) => {
   app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
   app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
 
+  // edit
+  app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
+  app.put('/admin/restaurants/:id', authenticatedAdmin, adminController.putRestaurant)
+
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
 

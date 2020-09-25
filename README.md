a0c0159390dcd3ddd8d0853d2b29b6dfb767bb32
STTSAI <shinting.tsai@gmail.com>
2020-09-25 23:14:28 +0800

    doc: add readme file

---
+++ README.md
@@ -0,0 +1,92 @@
+# 餐廳評論網 (Restaurant Forum)
+
+「餐廳評論網」的目標是幫助網站使用者完成以下事情：
+- 找到好餐廳
+- 查看餐廳的基本資訊
+
+除了網站本身會提供豐富的餐廳資訊，也會建立使用者評論與收藏等互動功能，累積使用者的活動數據，讓值得推薦的好餐廳浮上檯面。
+
+[DEMO](https://pure-tor-17450.herokuapp.com/)
+
+## 介面 (Snapshot)
+![image](https://i.imgur.com/35owPif.png)
+
+![image](https://i.imgur.com/UtWpZvF.png)
+
+
+## 功能 (Features)
+
+網站使用者有「一般使用者」和「網站管理員」兩種角色
+
+**前台**
+---------
+- 使用者可以註冊/登入/登出網站
+- 使用者可以在瀏覽所有餐廳與個別餐廳詳細資料
+- 在瀏覽所有餐廳資料時，可以用分類篩選餐廳
+- 使用者可以對餐廳留下評論
+- 使用者可以收藏餐廳
+- 使用者可以查看最新上架的 10 筆餐廳
+- 使用者可以查看最新的 10 筆評論
+- 使用者可以編輯自己的個人資料
+- 使用者可以查看自己評論過、收藏過的餐廳
+- 使用者可以追蹤其他的使用者
+- 使用者可以查看自己追蹤中的使用者與正在追蹤自己的使用者
+
+**後台**
+---------
+- 只有網站管理者可以登入網站後台
+- 網站管理者可以在後台管理餐廳的基本資料
+- 網站管理者可以在後台管理餐廳分類
+
+## 安裝與執行步驟 (Install)
+- 下載專案到本機
+```
+git clone https://github.com/ShinTingTsai/restaurant_forum.git
+```
+- 安裝套件
+```
+npm install
+```
+- 修改.env.example檔案內容：IMGUR_CLIENT_ID等欄位需輸入自己的資訊
+- 修改.env.example檔案名稱為.env
+- 建立種子資料
+```
+npm run seed
+```
+- 開啟程式
+```
+npm run dev
+```
+- 請至http://localhost:3000開始使用程式
+
+## 測試帳號 (Test Account)
+Ａdmin
+- email: root@example.com
+- password: 12345678
+User
+- email: user1@example.com
+- password: 12345678
+
+## 環境建置與需求 (Built with)
+- bcryptjs: v2.4.3
+- body-parser: v1.19.0
+- connect-flash: v0.1.1
+- dotenv: v8.2.0
+- Express v4.17.1
+- Express-Handlebars v4.0.4
+- Express-session: v1.17.1
+- faker: v5.1.0
+- imgur-node-api: v0.1.0
+- method-override: v3.0.0
+- moment: v2.28.0
+- multer: 1.4.2
+- mysql2: 2.1.0
+- Node.js v10.15.0
+- passport: v0.4.1
+- passport-local: v1.0.0
+- pg: v8.3.3
+- sequelize: v6.3.5
+- sequelize-cli: v6.2.0
+
+## 開發人員 (Author)
+Shin-Ting Tsai
--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,13 @@
 const express = require('express')
-const router = express.Router();
+const router = express.Router()
 const passport = require('../config/passport')
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
-const apiAdminController = require('../controllers/api/adminController.js')
 const userController = require('../controllers/userController.js')
 const categoryController = require('../controllers/categoryController.js')
 const commentController = require('../controllers/commentController.js')
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
-
 
 const authenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -31,35 +29,31 @@ const isOwner = (req, res, next) => {
   req.flash('error_messages', 'You are not authorized to do the action.')
   return res.redirect(`/users/${req.user.id}`)
 }
-// API
-router.get('/api/admin/restaurants', apiAdminController.getRestaurants)
+
 // Profile
 router.get('/users/top', authenticated, userController.getTopUser)
 router.get('/users/:id', authenticated, userController.getUser)
 router.get('/users/:id/edit', authenticated, isOwner, userController.editUser)
 router.put('/users/:id', authenticated, isOwner, upload.single('image'), userController.putUser)
-
-router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
-router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
-
+// like
 router.post('/like/:restaurantId', authenticated, userController.addLike)
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
-
+// following
 router.post('/following/:userId', authenticated, userController.addFollowing)
 router.delete('/following/:userId', authenticated, userController.removeFollowing)
-
-// Comment
+// comments
 router.post('/comments', authenticated, commentController.postComment)
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
-
-// Read
+// favorite
+router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
+router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
+// Read restaurants
 router.get('/restaurants/top', authenticated, restController.getTopRestaurants)
 router.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
-
 // Admin
 router.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
 router.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
@@ -78,7 +72,7 @@ router.post('/admin/categories', authenticatedAdmin, categoryController.postCate
 router.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
 router.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
 router.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
-
+// Account management
 router.get('/admin/users', authenticatedAdmin, adminController.getUsers)
 router.put('/admin/users/:id', authenticatedAdmin, adminController.putUsers)
 // SignIn/SignUp

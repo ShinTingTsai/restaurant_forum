73886615b2eb924ad60b0620901e5bc633042785
STTSAI <shinting.tsai@gmail.com>
2020-09-25 23:39:50 +0800

    refactor adminController.getRestaurants

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -1,17 +1,25 @@
+const { database } = require('faker')
 const db = require('../../models')
 const { Restaurant, Category, User } = db
+const adminService = require('../../services/adminService.js')
 
 
 const adminController = {
+  // getRestaurants: (req, res) => {
+  //   // 用{ raw: true }將資料轉換成ＪＳ原生物件
+  //   return Restaurant.findAll({
+  //     raw: true,
+  //     nest: true,
+  //     include: [Category]
+  //   }).then(restaurants => {
+  //     console.log(restaurants)
+  //     return res.json({ restaurants: restaurants })
+  //   })
+  // }
+
   getRestaurants: (req, res) => {
-    // 用{ raw: true }將資料轉換成ＪＳ原生物件
-    return Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: [Category]
-    }).then(restaurants => {
-      console.log(restaurants)
-      return res.json({ restaurants: restaurants })
+    adminService.getRestaurants(req, res, (data) => {
+      return res.json(data)
     })
   }
 }
